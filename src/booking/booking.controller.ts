409003665baf591e315ca8a42f83be30baee7900
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2023-12-27 19:59:12 +0900

    make cancel

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,9 +1,10 @@
 import {Roles} from 'src/auth/roles.decorator'
 import {RolesGuard} from 'src/auth/roles.guard'
 import {Role} from 'src/user/types/userRole.type'
-import {Req, Get, Post, UseGuards, Controller} from '@nestjs/common'
+import {Req, Param, Get, Post, Delete, UseGuards, Controller} from '@nestjs/common'
 import {BookingService} from './booking.service'
 import {BookingGuard} from './booking.guard'
+import {Id} from '../utils/id'
 
 @UseGuards(RolesGuard)
 @Controller('booking')
@@ -19,11 +20,19 @@ export class BookingController {
         await this.bookingService.book()
         return {message: '예매가 완료되었습니다.', count: 1, spending}
     }
+
+    // 내 예매 목록 확인
+    @Roles(Role.User)
+    @Get('myBooking')
+    async getMyBooking(@Req() req) {
+        return await this.bookingService.findByUser(req.user.id)
+    }
 	
-	// 내 예매 목록 확인
+	// 예매 취소
 	@Roles(Role.User)
-	@Get('myBooking')
-	async getMyBooking(@Req() req){
-		return await this.bookingService.findByUser(req.user.id)
+	@Delete(':id')
+	async cancel(@Param() param: Id){
+		const {id} = param
+		return await this.bookingService.cancel(id)
 	}
 }
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,4 +1,4 @@
-import {Scope, Inject, Injectable, BadRequestException, ConflictException} from '@nestjs/common'
+import {Scope, Inject, Injectable, BadRequestException, ConflictException, ForbiddenException} from '@nestjs/common'
 import {Repository, QueryFailedError} from 'typeorm'
 import {InjectRepository} from '@nestjs/typeorm'
 import {Booking} from './entities/booking.entity'
@@ -7,6 +7,7 @@ import {User} from '../user/entities/user.entity'
 import {DataSource} from 'typeorm'
 import {REQUEST} from '@nestjs/core'
 import {RequestWithInfo} from '../utils/request-with-info'
+import {ConcertService} from 'src/concert/concert.service'
 
 @Injectable({scope: Scope.REQUEST})
 export class BookingService {
@@ -18,7 +19,8 @@ export class BookingService {
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
         private readonly dataSource: DataSource,
-        @Inject(REQUEST) private req: RequestWithInfo
+        @Inject(REQUEST) private req: RequestWithInfo,
+		@Inject(ConcertService) private concertService: ConcertService
     ) {}
 
     // 공연 예매, 일단 1장씩만 가능
@@ -44,9 +46,35 @@ export class BookingService {
             await queryRunner.release()
         }
     }
+
+    // 내 예매 목록 확인
+    async findByUser(userId: number) {
+        return await this.bookingRepository.find({where: {userId}})
+    }
 	
-	// 내 예매 목록 확인
-	async findByUser(userId){
-		return await this.bookingRepository.find({where:{userId}})
+	// 예매 취소
+	async cancel(id: number){
+		const booking = await this.bookingRepository.findOne({where:{id}})
+		const {user} = this.req
+		if(!booking || booking.userId!==user.id) throw new ForbiddenException('권한이 없습니다.')
+		const queryRunner = this.dataSource.createQueryRunner()
+        await queryRunner.connect()
+        await queryRunner.startTransaction()
+		try {
+			const {scheduleId,spending} = booking
+			const schedule = await this.concertService.findSchedule(scheduleId)
+            user.point += spending
+			await queryRunner.manager.save(user)
+            await queryRunner.manager.delete(Booking,booking)
+            ++schedule.vacancy
+            await queryRunner.manager.save(schedule)
+            await queryRunner.commitTransaction()
+        } catch (e) {
+            await queryRunner.rollbackTransaction()
+            if (e instanceof QueryFailedError) throw new ConflictException('이미 예매하신 공연입니다.')
+            throw e
+        } finally {
+            await queryRunner.release()
+        }
 	}
 }
